1d9e1f9b547e264b36a6192266896fc346b4c512
KrabbiePatty <blakegateley@gmail.com>
2026-03-26 17:45:52 -0500

    Added the prompts for switch configuration

--- AutoConfigure.py
+++ AutoConfigure.py
@@ -33,5 +33,31 @@ if device.strip().lower() == "router":
             print("Invalid input entered. Enter either y or n. ")
     banner = input("What is the banner? ")
 
-        
+# SWITCH CONFIGURATION
+if device.strip().lower() == "switch":
+    hostname = input("What is the hostname for the switch? ")
+    secret = input("What is the Priv Exec secret? ")
+    while True:
+        lookup = input("Disable DNS lookup? y/n ")
+        if lookup == "y" or lookup == "n":
+            break
+        else:
+            print("Invalid input entered. Enter either y or n. ")
+    username = input("What is the username? ")
+    password = input("What is the password? ")
+    consolePass = input("Enter console line password or enter 'local' to enable login local: ")
+    vtyPass = input("Enter vty line password or enter 'local' to enable login local: ")
+    while True:
+        ssh = input("Enable SSH? y/n ")
+        if ssh == "y" or lookup == "n":
+            break
+        else:
+            print("Invalid input entered. Enter either y or n. ")
+    while True:
+        encrypt = input("Encrypt passwords? y/n ")
+        if encrypt == "y" or lookup == "n":
+            break
+        else:
+            print("Invalid input entered. Enter either y or n. ")
+    banner = input("What is the banner? ")
         
