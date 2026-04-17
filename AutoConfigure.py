2264fe034083c35d1ba5475b4c5c1d62b3be42fe
KrabbiePatty <blakegateley@gmail.com>
2026-04-17 05:23:03 -0500

    ADDED FEATURE: DHCP Configuration

--- AutoConfigure.py
+++ AutoConfigure.py
@@ -1,3 +1,6 @@
+import re
+ipv4_pattern = r"^(?:0|[1-9]\d?|1\d{2}|2[0-4]\d|25[0-5])(?:\.(?:0|[1-9]\d?|1\d{2}|2[0-4]\d|25[0-5])){3}$"
+
 def gatherData():
     # DEFINE EMPTY DICTIONARY
     Settings = {}
@@ -11,6 +14,66 @@ def gatherData():
     password = input("What is the password? ")
     consolePass = input("Enter console line password or enter 'local' to enable login local: ")
     vtyPass = input("Enter vty line password or enter 'local' to enable login local: ")
+    while True:
+        dhcp = input("Would you like to conifgure DHCP? y/n ")
+        if dhcp == "y":
+            dhcp = True
+            break
+        elif dhcp == "n":
+            dhcp == False
+            break
+        else:
+            print("ERROR: Invalid input detected. Please enter either 'y' or 'n'. ")
+    while True:
+            try:
+                while True:
+                    dhcpNumber = int(input("How many DHCP Pools are you configuring? "))
+                    if dhcpNumber <= 0:
+                        print("ERROR: Invalid input detected. Enter a number greater than 0. ")
+                    else:
+                          break
+                
+                break
+            except ValueError:
+                    print("ERROR: Invalid input detected. Enter an integer. ")
+    for i in range(dhcpNumber):
+        dhcpPool = input("What is the name of the DHCP Pool? ")
+        while True:
+            excludeStart = input("Where should the excluded address start? (xxx.xxx.xxx.xxx)")
+            if re.fullmatch(ipv4_pattern,excludeStart):
+                break
+            else:
+                print("ERROR: Input does not match the format for an ipv4 address.")
+        while True:
+            excludeEnd = input("Where should the excluded address end? (xxx.xxx.xxx.xxx)")
+            if re.fullmatch(ipv4_pattern,excludeEnd):
+                break
+            else:
+                print("ERROR: Input does not match the format for an ipv4 address.")
+        while True:
+            defaultRouter = input("What is the default router? (xxx.xxx.xxx.xxx)")
+            if re.fullmatch(ipv4_pattern,defaultRouter):
+                break
+            else:
+                print("ERROR: Input does not match the format for an ipv4 address.")
+        while True:
+            network = input("What is the network address? (xxx.xxx.xxx.xxx)")
+            subnet = input("What is the subnet mask? (xxx.xxx.xxx.xxx)")
+            if re.fullmatch(ipv4_pattern,network):
+                break
+            else:
+                print("ERROR: Input does not match the format for an ipv4 address.")
+            if re.fullmatch(ipv4_pattern,subnet):
+                break
+            else:
+                print("ERROR: Input does not match the format for a subnet mask.")
+        
+        Settings["dhcpPool" + str(i)] = dhcpPool
+        Settings["excludeStart" + str(i)] = excludeStart
+        Settings["excludeEnd" + str(i)] = excludeEnd
+        Settings["defaultRouter" + str(i)] = defaultRouter
+        Settings["network" + str(i)] = network
+        Settings["subnet" + str(i)] = subnet
 
     # ASSIGN GATHERED VALUES TO DICTIONARY
     Settings["hostname"] = hostname
@@ -24,8 +87,9 @@ def gatherData():
     
     # RETURN DICTIONARY
     return Settings
-    
 
+#TEST OUTPUT    
+#print(gatherData())
 
     
 
@@ -88,8 +152,28 @@ exit
 
         '''
     config = config + vtyConfig
+
+    dhcpNumber = 0
+    while True:
+        
+        if "dhcpPool"+ str(dhcpNumber) in settings:
+            dhcpConfig = f'''
+ip dhcp exclude {settings["excludeStart"+str(dhcpNumber)]} {settings["excludeEnd"+str(dhcpNumber)]} 
+ip dhcp pool {settings["dhcpPool"+str(dhcpNumber)]} 
+network {settings["network"+str(dhcpNumber)]} {settings["subnet"+str(dhcpNumber)]} 
+default-router {settings["defaultRouter"+str(dhcpNumber)]} 
+
+    '''
+            #DHCP CONFIGURATION
+            dhcpNumber += 1
+            config = config + dhcpConfig
+        else:
+            break
+
     return config
 
+
+# MAKE THE CONFIGURATION FILE
 def makeFile(string):
     fileName = input("What is the config file name? ") + ".txt"
     with open(fileName, "w") as file:
